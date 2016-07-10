695b2276b14afff1c1188f1fbceab816c157caff
z-gora <domdlapsa@gmail.com>
2016-07-10 23:33:44 +0200

    Redir dir in update

--- update.php
+++ update.php
@@ -1,6 +1,6 @@
 <?php
     file_put_contents("dictionary.db", file_get_contents("http://wyd-dict.tk/dictionary.db"));
     echo "OK!";
-    header("Location: /");
+    header("Location: ".substr($_SERVER['PHP_SELF'], 0, strlen($_SERVER['PHP_SELF'])-10));
     die();
 ?>
